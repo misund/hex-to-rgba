3ab2a45e87ad8183b9889a6dc7c5f6d4dba9a8cb
misund <thomas@hemmeligadresse.com>
2017-09-30 21:52:34 +0200

    Lint

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const fixHex = (hex) => {
+  let newHex = hex.startsWith('#') ? hex.slice(1) : hex;
+
+  if (newHex.length === 3) {
+    newHex = `${newHex.slice(0, 1)}${newHex.slice(0, 1)}${newHex.slice(1, 2)}${newHex.slice(1, 2)}${newHex.slice(2, 3)}${newHex.slice(2, 3)}`;
+  }
+
+  return newHex;
+};
+
+const isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n);
+
 /**
  * Turns an old-fashioned css hex color value into a rgb color value.
  *
@@ -7,18 +19,7 @@
  * @param An alpha value to apply. (optional) ('0.5', '0.25')
  * @return An rgb or rgba value. ('rgb(11, 22, 33)'. 'rgba(11, 22, 33, 0.5)')
  */
-const hexToRgba = function(hex, a) {
-  const fixHex = (hex) => {
-    let newHex = hex.startsWith('#') ? hex.slice(1) : hex;
-
-    if (newHex.length === 3) {
-      newHex = `${newHex.slice(0, 1)}${newHex.slice(0, 1)}${newHex.slice(1, 2)}${newHex.slice(1, 2)}${newHex.slice(2, 3)}${newHex.slice(2, 3)}`;
-    }
-
-
-    return newHex;
-  };
-
+const hexToRgba = (hex, a) => {
   const newHex = fixHex(hex);
   const r = parseInt(newHex.substring(0, 2), 16);
   const g = parseInt(newHex.substring(2, 4), 16);
@@ -26,13 +27,11 @@ const hexToRgba = function(hex, a) {
 
   let o;
   if (newHex.length === 8) {
-    o = +((parseInt(newHex.substring(6, 8), 16)/255).toFixed(2));
+    o = +((parseInt(newHex.substring(6, 8), 16) / 255).toFixed(2));
   }
   o = isNumeric(a) ? a : o;
 
   return isNumeric(o) ? `rgba(${r}, ${g}, ${b}, ${o})` : `rgb(${r}, ${g}, ${b})`;
 };
-
-const isNumeric = n => !isNaN(parseFloat(n)) && isFinite(n);
 
 module.exports = hexToRgba;
--- test/test.js
+++ test/test.js
@@ -1,176 +1,176 @@
+/* global describe it */
 import assert from 'assert';
 import hexToRgba from '..';
 
-describe('hex-to-rgba', function() {
-  describe('6-digit hex values, no a', function() {
-    it('should calculate correct rgb values', function() {
+describe('hex-to-rgba', () => {
+  describe('6-digit hex values, no a', () => {
+    it('should calculate correct rgb values', () => {
       assert.equal('rgb(17, 34, 51)', hexToRgba('112233'));
     });
 
-    it('should ignore a leading hash sign', function() {
+    it('should ignore a leading hash sign', () => {
       assert.equal('rgb(17, 34, 51)', hexToRgba('#112233'));
     });
 
-    it('should correctly calculate uppercase hex', function() {
+    it('should correctly calculate uppercase hex', () => {
       assert.equal('rgb(127, 127, 127)', hexToRgba('#7F7F7F'));
     });
 
-    it('should correctly calculate lowercase hex', function() {
+    it('should correctly calculate lowercase hex', () => {
       assert.equal('rgb(127, 127, 127)', hexToRgba('#7f7f7f'));
     });
   });
 
-  describe('6-digit hex values, a as parameter', function() {
-    it('should calculate rgba values from hex and string alpha value', function() {
+  describe('6-digit hex values, a as parameter', () => {
+    it('should calculate rgba values from hex and string alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.5)', hexToRgba('112233', '0.5'));
     });
 
-    it('should calculate rgba values from hex and numerical alpha value', function() {
+    it('should calculate rgba values from hex and numerical alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.75)', hexToRgba('112233', 0.75));
     });
 
-    it('should handle the edge case where alpha value is 1', function() {
+    it('should handle the edge case where alpha value is 1', () => {
       assert.equal('rgba(17, 34, 51, 1)', hexToRgba('112233', 1));
     });
 
-    it('should handle the edge case where alpha value is 0', function() {
+    it('should handle the edge case where alpha value is 0', () => {
       assert.equal('rgba(17, 34, 51, 0)', hexToRgba('112233', 0));
     });
 
-    it('should calculate rgba values from hex with leading hash and string alpha value', function() {
+    it('should calculate rgba values from hex with leading hash and string alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.5)', hexToRgba('#112233', '0.5'));
     });
 
-    it('should calculate rgba values from hex with leading hash and numerical alpha value', function() {
+    it('should calculate rgba values from hex with leading hash and numerical alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.75)', hexToRgba('#112233', 0.75));
     });
   });
 
-  describe('3-digit hex values, no a', function() {
-    it('should calculate correct rgb values', function() {
+  describe('3-digit hex values, no a', () => {
+    it('should calculate correct rgb values', () => {
       assert.equal('rgb(17, 34, 51)', hexToRgba('123'));
     });
 
-    it('should ignore a leading hash sign', function() {
+    it('should ignore a leading hash sign', () => {
       assert.equal('rgb(17, 34, 51)', hexToRgba('#123'));
     });
   });
 
-  describe('3-digit hex values, a as parameter', function() {
-    it('should calculate rgba values from hex and string alpha value', function() {
+  describe('3-digit hex values, a as parameter', () => {
+    it('should calculate rgba values from hex and string alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.5)', hexToRgba('123', '0.5'));
-    })
+    });
 
-    it('should calculate rgba values from hex and numerical alpha value', function() {
+    it('should calculate rgba values from hex and numerical alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.75)', hexToRgba('123', 0.75));
     });
 
-    it('should handle the edge case where alpha value is 1', function() {
+    it('should handle the edge case where alpha value is 1', () => {
       assert.equal('rgba(17, 34, 51, 1)', hexToRgba('123', 1));
     });
 
-    it('should handle the edge case where alpha value is 0', function() {
+    it('should handle the edge case where alpha value is 0', () => {
       assert.equal('rgba(17, 34, 51, 0)', hexToRgba('123', 0));
     });
 
-    it('should calculate rgba values from hex with leading hash and string alpha value', function() {
+    it('should calculate rgba values from hex with leading hash and string alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.5)', hexToRgba('#123', '0.5'));
     });
 
-    it('should calculate rgba values from hex with leading hash and numerical alpha value', function() {
+    it('should calculate rgba values from hex with leading hash and numerical alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.75)', hexToRgba('#123', 0.75));
     });
   });
 
-  describe('8-digit hex values, no a', function() {
-    it('should calculate correct rgb values', function() {
+  describe('8-digit hex values, no a', () => {
+    it('should calculate correct rgb values', () => {
       assert.equal('rgba(17, 34, 51, 0.27)', hexToRgba('11223344'));
     });
 
-    it('should ignore a leading hash sign', function() {
+    it('should ignore a leading hash sign', () => {
       assert.equal('rgba(17, 34, 51, 0.27)', hexToRgba('#11223344'));
     });
 
-    it('should remove trailing zeros', function() {
+    it('should remove trailing zeros', () => {
       assert.equal('rgba(17, 34, 51, 0.5)', hexToRgba('1122337f'));
     });
 
-    it('should handle the edge case where alpha value is 1', function() {
+    it('should handle the edge case where alpha value is 1', () => {
       assert.equal('rgba(17, 34, 51, 1)', hexToRgba('112233ff'));
     });
 
-    it('should handle the edge case where alpha value is 0', function() {
+    it('should handle the edge case where alpha value is 0', () => {
       assert.equal('rgba(17, 34, 51, 0)', hexToRgba('112233', 0));
     });
-
   });
 
-  describe('8-digit hex values, a as parameter (separate parameter should override alpha in hex)', function() {
-    it('should calculate rgba values from hex and string alpha value', function() {
+  describe('8-digit hex values, a as parameter (separate parameter should override alpha in hex)', () => {
+    it('should calculate rgba values from hex and string alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.5)', hexToRgba('11223344', '0.5'));
-    })
+    });
 
-    it('should calculate rgba values from hex and numerical alpha value', function() {
+    it('should calculate rgba values from hex and numerical alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.75)', hexToRgba('11223344', 0.75));
     });
 
-    it('should handle the edge case where alpha value is 1', function() {
+    it('should handle the edge case where alpha value is 1', () => {
       assert.equal('rgba(17, 34, 51, 1)', hexToRgba('11223344', 1));
     });
 
-    it('should handle the edge case where alpha value is 0', function() {
+    it('should handle the edge case where alpha value is 0', () => {
       assert.equal('rgba(17, 34, 51, 0)', hexToRgba('11223344', 0));
     });
 
-    it('should calculate rgba values from hex with leading hash and string alpha value', function() {
+    it('should calculate rgba values from hex with leading hash and string alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.5)', hexToRgba('#11223344', '0.5'));
     });
 
-    it('should calculate rgba values from hex with leading hash and numerical alpha value', function() {
+    it('should calculate rgba values from hex with leading hash and numerical alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.75)', hexToRgba('#11223344', 0.75));
     });
   });
 
-  describe.skip('4-digit hex values, no a', function() {
-    it('should calculate correct rgb values', function() {
+  describe.skip('4-digit hex values, no a', () => {
+    it('should calculate correct rgb values', () => {
       assert.equal('rgba(17, 34, 51, 0.27)', hexToRgba('1234'));
     });
 
-    it('should ignore a leading hash sign', function() {
+    it('should ignore a leading hash sign', () => {
       assert.equal('rgb(17, 34, 51, 0.27)', hexToRgba('#1234'));
     });
 
-    it('should handle the edge case where alpha value is 1', function() {
+    it('should handle the edge case where alpha value is 1', () => {
       assert.equal('rgba(17, 34, 51, 1)', hexToRgba('123f'));
     });
 
-    it('should handle the edge case where alpha value is 0', function() {
+    it('should handle the edge case where alpha value is 0', () => {
       assert.equal('rgba(17, 34, 51, 0)', hexToRgba('1230'));
     });
   });
 
-  describe.skip('4-digit hex values, a as parameter (separate parameter should override alpha in hex)', function() {
-    it('should calculate rgba values from hex and string alpha value', function() {
+  describe.skip('4-digit hex values, a as parameter (separate parameter should override alpha in hex)', () => {
+    it('should calculate rgba values from hex and string alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.5)', hexToRgba('1234', '0.5'));
-    })
+    });
 
-    it('should calculate rgba values from hex and numerical alpha value', function() {
+    it('should calculate rgba values from hex and numerical alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.75)', hexToRgba('1234', 0.75));
     });
 
-    it('should handle the edge case where alpha value is 1', function() {
+    it('should handle the edge case where alpha value is 1', () => {
       assert.equal('rgba(17, 34, 51, 1)', hexToRgba('1234', 1));
     });
 
-    it('should handle the edge case where alpha value is 0', function() {
+    it('should handle the edge case where alpha value is 0', () => {
       assert.equal('rgba(17, 34, 51, 0)', hexToRgba('1234', 0));
     });
 
-    it('should calculate rgba values from hex with leading hash and string alpha value', function() {
+    it('should calculate rgba values from hex with leading hash and string alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.5)', hexToRgba('#1234', '0.5'));
     });
 
-    it('should calculate rgba values from hex with leading hash and numerical alpha value', function() {
+    it('should calculate rgba values from hex with leading hash and numerical alpha value', () => {
       assert.equal('rgba(17, 34, 51, 0.75)', hexToRgba('#1234', 0.75));
     });
   });
